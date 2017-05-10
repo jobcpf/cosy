af4c81db18f931173aee2342fe96d0d6b525c71e
jobcpf <oliver.blakeman@carbonprojectfinance.co.uk>
2017-05-10 09:18:34 +0100

    First commit

---
+++ cosy/data/data.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""
+Module ...
+
+@Author: 
+@Date: 
+
+"""
+
+################## Packages #################################### Packages #################################### Variables ##################
+
+# Standard import
+import os
+
+# Import custom modules
+
+
+################## Variables #################################### Variables #################################### Variables ##################
+
+from [project_name].global_config import * # get global variables
+script_file = "%s: %s" % (now_file,os.path.basename(__file__))
+
+################## Functions ###################################### Functions ###################################### Functions ####################
+
+    
+
+
+
+
+
+
